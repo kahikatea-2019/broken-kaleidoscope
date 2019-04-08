07d4cd55111a8c92d3119f13574676286f7cbf25
Struan Wall <struan03@hotmail.com>
2019-04-09 10:55:39 +1200

    got a pixel

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+  render () {
+    return (
+
+      <div style={{
+        fontFamily: 'Times New Roman',
+        height: '100px',
+        width: '100px',
+        backgroundColor: 'blue'
+      }} ></div>
+    )
+  }
+}
+
+export default Pixel
