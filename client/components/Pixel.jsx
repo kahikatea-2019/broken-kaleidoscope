c9d65d443b38a96f3a0799a1eb44235f0233e91c
Yunan Li <amy_tako@hotmail.com>
2019-04-09 10:56:37 +1200

    add the first squred

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Pixel = () => {
+  return (
+    <div id='app'>
+      <div style={{
+        fontFamily: 'Times New Roman',
+        height: '80px',
+        width: '80px',
+        backgroundColor: 'cornflowerblue'
+      }}></div>
+    </div>
+
+  )
+}
+
+export default Pixel
