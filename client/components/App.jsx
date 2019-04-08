ff1b353a262d21cd5b20889c8db378d7f8ead966
Bradley Wheeler <bradley.wh@outlook.com>
2019-04-09 11:07:53 +1200

    change pixel color on click

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,6 @@ import Pixel from './Pixel'
 const App = () => {
   return (
     <React.Fragment>
-      <div>React development has begun!</div>
       <Pixel />
       <Pixel />
       <Pixel />
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -6,15 +6,29 @@ class Pixel extends React.Component {
 
     this.state = {
       style: {
-        height: '20px',
-        width: '20px',
-        backgroundColor: 'cyan'
+        height: '90px',
+        width: '90px',
+        backgroundColor: this.randomHexColor()
       }
     }
   }
+
+  randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+  clickHandler = evt => {
+    this.setState({
+      style: {
+        height: '90px',
+        width: '90px',
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
+
   render () {
     return (
-      <div style={this.state.style}></div>
+      <div style={this.state.style} onClick={this.clickHandler}></div>
     )
   }
 }
