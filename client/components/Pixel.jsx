b19cbc24b873dee9e7b4526cc5c6a8455609fdaa
Anandani Mala Prakash <anandaniprakash@icloud.com>
2019-04-09 15:27:27 +1200

    Rainbow pixels

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 class Pixel extends React.Component {
   constructor () {
     super()
 
-    this.state.style = {
+    this.state = {
       style: {
-        backgroundColor: 'blue',
+        backgroundColor: randomHexColor(),
         width: '50px',
         height: '50px'
+
       }
+
     }
   }
   render () {
     return (
       <React.Fragment>
-        <div style ={this.state.style}></div>
+        <div style ={this.state.style}>yas</div>
 
       </React.Fragment>
     )
