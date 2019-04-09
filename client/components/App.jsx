486d0226f465f5d4f5d744546c84d975f89679d7
Azaan Virk <azaan.virk1@gmail.com>
2019-04-09 17:11:59 +1200

    All working here

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import Pixel from './Pixel.jsx'
 
-const App = () => {
-  return (
-    <div>
-      <Pixel />
-    </div>
-
-  )
+const App = () => { 
+  return Array.from({ length: 1000 }, () => <Pixel />)
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,15 +1,74 @@
 import React from 'react'
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
 class Pixel extends React.Component {
+  constructor () {
+    super()
+
+    const randomHexColor = () =>
+      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+    this.state = {
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: randomHexColor()
+      }
+    }
+  }
+
+  colorSplash= () => {
+    this.setState({
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: randomHexColor()
+      }
+    })
+  }
+
+  dubClick= () => {
+    this.setState({
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'white'
+      }
+    })
+  }
+
+  changeColor = () => {
+    this.setState({
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: randomHexColor()
+      }
+    })
+  }
+
+  draggy = () => {
+    this.setState({
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'pink'
+      }
+    })
+  }
+
   render () {
     return (
-
-      <div style={{
-        fontFamily: 'Times New Roman',
-        height: '100px',
-        width: '100px',
-        backgroundColor: 'blue'
-      }} ></div>
+      <div style={this.state.style} onClick={this.changeColor}
+        onDoubleClick={this.dubClick}onMouseEnter={this.colorSplash}onDragEnter={this.draggy}>
+      </div>
     )
   }
 }
