bdc6689f8d6a1ff3b7416b90dad16c79513702b0
Bradley Wheeler <bradley.wh@outlook.com>
2019-04-09 20:53:35 +1200

    🌴🌴🌴🌴

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+constructor() {
+    super()
+}
+
+render() {
+    return(
+        <div style={{
+            backgroundColor: 'yellow',
+            height: '20px',
+            width: '20px'
+        }}>
+        </div>
+    )
+  }
+}
+
+export default Pixel
