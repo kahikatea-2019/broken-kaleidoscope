96d04ce9d56a299aaba79f1a6287cb4035ead0fa
MurdocAV <azaan.virk1@gmail.com>
2020-02-03 15:01:26 +1300

    Added styling to Pixel Component

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default class Welcome extends React.Component {
   render() {
     return (
-      <div className="pixel"></div>
+      <div className="pixel"
+        style={{
+        fontFamily: 'Times New Roman',
+        height: '40px',
+        width: '40px',
+        backgroundColor: 'Pink'
+        }}
+      >
+      </div>
     )
   }
 }
