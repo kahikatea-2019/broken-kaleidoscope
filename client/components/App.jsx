78c2272244f345d0485986acfd74746fa7df514b
MurdocAV <azaan.virk1@gmail.com>
2020-02-04 16:06:17 +1300

    rendering from a function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,37 +1,18 @@
 import React from 'react'
 import Pixel from './Pixel'
 
+
+const multiplyPixel = () => {
+  let ammountOfPixels = 100
+  let pixels = Array.from({length: ammountOfPixels}, (v, i) => <Pixel key={i}></Pixel>);
+  // console.log(pixels)
+  return pixels
+}
+
 const App = () => {
   return (
     <React.Fragment>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
-      <Pixel></Pixel>
+      {multiplyPixel()}
     </React.Fragment>
   )
 }
